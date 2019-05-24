d2a8d1fb4dc70de7ff8c037b75a6bf549a82c19d
Connor Jones <ConnorLeeJones@gmail.com>
2019-05-23 21:43:39 -0400

    Completed lab

--- Main.java
+++ Main.java
@@ -5,29 +5,39 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Main
-{
-    // instance variables - replace the example below with your own
-    private int x;
+import java.util.Scanner;
+import java.util.Random; 
 
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+public class Main {
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args ){
+        Scanner scnr = new Scanner(System.in);
+        Random rando = new Random();
+        int answer = rando.nextInt(100) + 1;
+        int lastGuess;
+        int count = 1;
+        
+        System.out.println("Guess a number between 1 and 100 (inclusive)");
+        int guess = scnr.nextInt();
+        
+        while (guess != answer) {
+           
+            if (guess < answer) {
+                System.out.println("Too small");
+            } else {
+                System.out.println("Too large");
+            }
+            
+            System.out.println("Current number of guesses: " + count);
+            
+            lastGuess = guess;
+            guess = scnr.nextInt();
+            
+            if (lastGuess != guess){
+                count++;
+            } 
+        }
+        System.out.println("That's correct! It only took you " + count + " guesses. Thanks for playing!");
     }
 }
+
